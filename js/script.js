e52c12fd003fd2ab3a61e8ce36abf23a034a1499
Marat <goofzilla365@mail.ru>
2019-02-26 18:14:52 +0300

    Предзащита-3 КОМЕТАРИИ
    с комментариями

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 var link = document.querySelector(".button-feedback");
-    var popup = document.querySelector(".overlay-modal");
-    var close = document.querySelector(".modal-close");
-    var login = popup.querySelector("[name=name]");
+var popup = document.querySelector(".overlay-modal");
+var close = document.querySelector(".modal-close");
+var login = popup.querySelector("[name=name]");
 
-    link.addEventListener("click", function (evt) {
-      evt.preventDefault();
-      popup.classList.add("overlay-modal-show");
-      login.focus();
-    });
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("overlay-modal-show");
+  login.focus();
+});
 
-    close.addEventListener("click", function (evt) {
-      evt.preventDefault();
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("overlay-modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("overlay-modal-show")) {
       popup.classList.remove("overlay-modal-show");
-    });
-
-    window.addEventListener("keydown", function (evt) {
-      if (evt.keyCode === 27) {
-        evt.preventDefault();
-        if (popup.classList.contains("overlay-modal-show")) {
-          popup.classList.remove("overlay-modal-show");
-        }
-      }
-    });
+    }
+  }
+});
