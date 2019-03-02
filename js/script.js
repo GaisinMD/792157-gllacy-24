04cb0f5a56b1820045b120d907d160d3b82526d6
Marat <goofzilla365@mail.ru>
2019-03-02 14:14:37 +0300

    Защита-2

--- js/script.js
+++ js/script.js
@@ -1,17 +1,22 @@
 var link = document.querySelector(".button-feedback");
 var popup = document.querySelector(".overlay-modal");
 var close = document.querySelector(".modal-close");
-var login = popup.querySelector("[name=name]");
+var byname = popup.querySelector(".feedback-item-name");
+var userMail = popup.querySelector(".feedback-item-mail");
+var message = popup.querySelector(".feedback-item-text");
+var form = popup.querySelector(".feedback-form");
+var sectionForm = popup.querySelector(".modal-feedback-form");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("overlay-modal-show");
-  login.focus();
+  byname.focus();
 });
 
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("overlay-modal-show");
+  sectionForm.classList.remove("modal-feedback-form-error");
 });
 
 window.addEventListener("keydown", function (evt) {
@@ -19,10 +24,19 @@ window.addEventListener("keydown", function (evt) {
     evt.preventDefault();
     if (popup.classList.contains("overlay-modal-show")) {
       popup.classList.remove("overlay-modal-show");
+      sectionForm.classList.remove("modal-feedback-form-error");
     }
   }
 });
 
+form.addEventListener("submit", function (evt) {
+  if (!byname.value || !userMail.value || !message.value) {
+    evt.preventDefault();
+    sectionForm.classList.remove("modal-feedback-form-error");
+    sectionForm.offsetWidth = sectionForm.offsetWidth;
+    sectionForm.classList.add("modal-feedback-form-error");
+  }
+});
 
 ymaps.ready(init);
 
@@ -31,13 +45,12 @@ function init() {
       center: [59.938631, 30.323055],
       zoom: 17,
       controls: ['smallMapDefaultSet']
-    }),    
+    }),
 
     myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
       hintContent: 'Магазин мороженного Gllacy',
       balloonContent: 'Добро пожаловать!'
-    }, 
-    {
+    }, {
       iconLayout: 'default#image',
       iconImageHref: 'img/pin.svg',
       iconImageSize: [80, 140],
